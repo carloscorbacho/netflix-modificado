c4334d988b1e3643ab1282b3d5af48ab9a812363
Carlos Corbacho <carlos.corbacho.padilla@gmail.com>
2023-10-14 19:40:04 +0200

    Responsive y cambios en el buscador

---
+++ src/components/home/Footer.jsx
@@ -0,0 +1,12 @@
+//MaterialUI
+import {Grid, Typography} from "@mui/material";
+
+export const Footer = () => {
+    return (
+        <Grid className="footer">
+            <Grid className="container-copy">
+                <Typography variant="p">© 2023 - Carlos Corbacho</Typography>
+            </Grid>
+        </Grid>
+    )
+}
